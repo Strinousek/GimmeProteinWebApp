0e8faf447f0582196b5b9557fb9c786d04347199
Vladimír Mrkáček <vlad.mrkacek@gmail.com>
2023-01-20 23:53:22 +0100

    Added masstransit rabbitmq to startup

--- src/warehouse/Warehouse.Api/Program.cs
+++ src/warehouse/Warehouse.Api/Program.cs
@@ -1,3 +1,5 @@
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMassTransit(x => x.UsingRabbitMq((context, cfg) =>
+{
+    cfg.Host(builder.Configuration["RabbitMq:Host"], Convert.ToUInt16(builder.Configuration["RabbitMq:Port"]), builder.Configuration["RabbitMq:VirtualHost"], h =>
+    {
+
+        h.Username(builder.Configuration["RabbitMq:User"]);
+        h.Password(builder.Configuration["RabbitMq:Password"]);
+    });
+
+    cfg.ConfigureEndpoints(context);
+}));
 
 var app = builder.Build();
 
